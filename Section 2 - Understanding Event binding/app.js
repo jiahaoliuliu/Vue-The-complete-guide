5bdae85528c0e8e64001e6b2d299c08be5bd4301
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-06 15:04:03 +0400

    Handling the key event modifiers and event binding and arguments

--- Section 2 - Understanding Event binding/app.js
+++ Section 2 - Understanding Event binding/app.js
@@ -1,9 +1,28 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-    };
-  },
+    data() {
+        return {
+            counter: 0,
+            name: '',
+            confirmedName: ''
+        };
+    },
+    methods: {
+        addCounter(num) {
+            this.counter = this.counter + num;
+        },
+        removeCounter(num) {
+            this.counter = this.counter - num;
+        },
+        setName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName
+        },
+        submitForm(event) {
+            alert('Submitted!');
+        },
+        confirmInput() {
+            this.confirmedName = this.name
+        }
+    },
 });
 
 app.mount('#events');
