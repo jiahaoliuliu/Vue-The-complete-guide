ab901c24e47b8c2a8a895df5fbcb1d911af50df1
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-19 22:15:06 +0400

    Set up a timer to reset counter to 0 after 2 seconds if it exceeds 50

--- Section 2/Section 2 - Understanding Event binding/app.js
+++ Section 2/Section 2 - Understanding Event binding/app.js
@@ -23,6 +23,15 @@ const app = Vue.createApp({
         //         this.fullname = this.name + ' ' + value;
         //     }
         // }
+        counter(value) {
+            // Start a timer of 2 seconds that resets counter if the value execeds 50
+            if (value > 50) {
+                const that = this;
+                setTimeout(function() {
+                    that.counter = 0;
+                }, 2000)
+            }
+        }
     },
     computed: {
         fullname() {
