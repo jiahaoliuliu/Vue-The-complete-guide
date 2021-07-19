a9b9b61729210b1cce0760026bbdde28ee3948d7
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-19 22:00:45 +0400

    Using watchers to calculate the value for fullname

--- Section 2/Section 2 - Understanding Event binding/app.js
+++ Section 2/Section 2 - Understanding Event binding/app.js
@@ -2,17 +2,28 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 10,
-            name: ''
+            name: '',
+            fullname: ''
         };
     },
-    computed: {
-        fullname() {
-            console.log("Running again in computed properties");
-            if (this.name === '') {
-                return '';
+    watch: {
+        // Whenever the data 'name' changes, this method will be executed
+        name(value) {
+            if (value === '') {
+                this.fullname = ''
+            } else {
+                this.fullname = value + ' ' + 'Liu';
             }
-            return this.name + " liu";
         }
+    },
+    computed: {
+        // fullname() {
+        //     console.log("Running again in computed properties");
+        //     if (this.name === '') {
+        //         return '';
+        //     }
+        //     return this.name + " liu";
+        // }
     },
     methods: {
         addCounter(num) {
