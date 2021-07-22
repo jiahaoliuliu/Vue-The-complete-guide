bdaaa5e8f37623d3649fee4e9b7bf93bb5c06cff
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-22 23:30:27 +0400

    Added App.vue. Set up the main.js

---
+++ Section 7 - Moving to a better development setup/Section 7 - Initial project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+// The default element to be exported
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 45678 900',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '9876 5432 12',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
+    }
+};
+</script>
