2ad969dfbf03e6d94e1192303d520bc3db55f9a6
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-27 02:06:30 +0400

    Finished the assignment 6

---
+++ Section 8 - Component communication/Section 8 - Assignment 6/src/components/ActiveUser.vue
@@ -0,0 +1,14 @@
+// ActiveUser should output a username (h2) and age (h3)
+
+<template>
+    <div>
+        <h2>User name: {{name}}</h2>
+        <h3>Age: {{age}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'age']
+}
+</script>
