8e1388fb796d6b1a93ec1a8349f09768a95241ba
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-30 01:46:52 +0400

    Registering the user info and the badge list as local component

--- Section 9 - Diving deeper into components/Section 9.1 - initial project/src/App.vue
+++ Section 9 - Diving deeper into components/Section 9.1 - initial project/src/App.vue
@@ -12,9 +12,11 @@
 
 <script>
 import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
 
 export default {
-  components: {'the-header': TheHeader},
+  components: {TheHeader, BadgeList, UserInfo },
   data() {
     return {
       activeUser: {
