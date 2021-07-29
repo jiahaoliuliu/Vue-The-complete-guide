c79b56f5086f1fbc4aba563ef6e15ba47697175b
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-07-30 01:44:45 +0400

    Registering the component called the header locally

--- Section 9 - Diving deeper into components/Section 9.1 - initial project/src/App.vue
+++ Section 9 - Diving deeper into components/Section 9.1 - initial project/src/App.vue
@@ -11,7 +11,10 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: {'the-header': TheHeader},
   data() {
     return {
       activeUser: {
